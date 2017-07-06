684780866d48ad66b8f577627b31bf88c10c0e44
organism <neuqzhj@live.com>
2017-07-06 12:24:43 +0800

    feature: Change to two cat to click

--- js/script.js
+++ js/script.js
@@ -1,8 +1,18 @@
-var catClickCount = 0;
-var clickNum = document.getElementById('click-number');
-clickNum.innerText = '0';
+var catClickCount = {};
 
-document.getElementById('cat-clicker').addEventListener('click', function () {
-    catClickCount++;
-    clickNum.innerText = catClickCount;
-}, true);
+(function initCatClicker() {
+    var catBoxed = document.getElementsByClassName('cat-box');
+
+    for (var i = 0; i < catBoxed.length; i++) {
+        var element = catBoxed[i];
+        catClickCount[element.getAttribute('id')] = 0;
+        element.getElementsByClassName('text-box')[0].innerText = '0';
+
+        element.addEventListener('click', function (e) {
+            var id = this.getAttribute('id');
+            var count = catClickCount[id] + 1;
+            catClickCount[id] = count;
+            this.getElementsByClassName('text-box')[0].innerText = count.toString();
+        }, false);
+    }
+})();

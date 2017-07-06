1e7fd36c65a711cb05c4239f1b84bfe0276e938f
organism <neuqzhj@live.com>
2017-07-06 13:49:31 +0800

    feature: Use jQuery and dynamically create elements

--- js/script.js
+++ js/script.js
@@ -1,18 +1,41 @@
 var catClickCount = {};
 
 (function initCatClicker() {
-    var catBoxed = document.getElementsByClassName('cat-box');
 
-    for (var i = 0; i < catBoxed.length; i++) {
-        var element = catBoxed[i];
-        catClickCount[element.getAttribute('id')] = 0;
-        element.getElementsByClassName('text-box')[0].innerText = '0';
+    var cats = [{
+            name: 'left',
+            image: 'images/cat.jpg'
+        },
+        {
+            name: 'right',
+            image: 'images/cat.jpg'
+        }
+    ];
 
-        element.addEventListener('click', function (e) {
-            var id = this.getAttribute('id');
-            var count = catClickCount[id] + 1;
-            catClickCount[id] = count;
-            this.getElementsByClassName('text-box')[0].innerText = count.toString();
-        }, false);
-    }
+    var formatCatClicker = '<div class="container">' +
+        '<div id="%cat%" class="cat-box">' +
+        '<div class="text-box">%count%</div>' +
+        '<img class="cat" src="%image%" alt="cute cat">' +
+        '</div>' +
+        '<p>%catName%</p>' +
+        '</div>';
+
+    var catElements = [];
+    cats.forEach(function (cat) {
+        var element = formatCatClicker.replace('%cat%', cat.name)
+            .replace('%count%', 0)
+            .replace('%image%', cat.image)
+            .replace('%catName%', cat.name);
+
+        catElements.push(element);
+        catClickCount[cat.name] = 0;
+    });
+
+    var container = $('.cat-clicker');
+    container.html(catElements.join(''));
+    container.on('click', '.cat-box', function (event) {
+        var id = this.getAttribute('id');
+        var count = ++catClickCount[id];
+        this.getElementsByClassName('text-box')[0].innerText = count.toString();
+    });
 })();

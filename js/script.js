00e6c8a4bf7aac7e05e97d3f56ac74a1ef6c49db
organism <neuqzhj@live.com>
2017-07-09 18:55:37 +0800

    feature: Implement with Knockout.js

--- js/script.js
+++ js/script.js
@@ -1,208 +1,105 @@
-(function () {
-    var data = {
-        cats: [{
+var appViewModel = function () {
+    var self = this;
+    this.elemInputName = document.getElementById('set-name');
+    this.elemInputImage = document.getElementById('set-image');
+    this.elemInputCount = document.getElementById('set-count');
+
+    this.catList = ko.observableArray(
+        [
+            ko.observable({
                 name: 'Black And White Kitten On Brown Textile',
                 image: 'images/black-and-white-kitten-on-brown-textile.jpg',
                 clickCount: 0
-            },
-            {
+            }),
+            ko.observable({
                 name: 'Eyes On You Cat',
                 image: 'images/eyes-on-you-cat.jpg',
                 clickCount: 0
-            },
-            {
+            }),
+            ko.observable({
                 name: 'Grey And White Short Fur Cat',
                 image: 'images/grey-and-white-short-fur-cat.jpg',
                 clickCount: 0
-            },
-            {
+            }),
+            ko.observable({
                 name: 'Kittens Rush',
                 image: 'images/kittens-cat-cat-puppy-rush.jpg',
                 clickCount: 0
-            },
-            {
+            }),
+            ko.observable({
                 name: 'Silver Tabby Cat',
                 image: 'images/silver-tabby-cat.jpg',
                 clickCount: 0
-            },
-            {
+            }),
+            ko.observable({
                 name: 'Two Yellow Cat',
                 image: 'images/two-yellow-cat.jpg',
                 clickCount: 0
-            }
-        ],
-        selectedCat: 0,
-        bShowAdmin: false
-    };
+            })
+        ]
+    );
 
-    var octopus = {
-        init: function () {
-            viewCatList.init();
-            viewCatDetail.init();
-            viewAdmin.init();
-        },
+    this.currentCat = ko.observable(0);
+    this.currentCat.subscribe(function () {
+        self.showAdmin(false);
+    });
 
-        getAllCats: function () {
-            return data.cats;
-        },
+    this.currentCatObject = ko.computed(function () {
+        return self.catList()[self.currentCat()]();
+    });
 
-        chooseCat: function (index) {
-            data.selectedCat = index;
-            viewCatDetail.render();
-            if (this.isShowAdmin() === true) {
-                this.showAdmin(false);
-            }
-        },
+    this.showAdmin = ko.observable(false);
+    this.showAdmin.subscribe(function (bShow) {
+        if (bShow) {
+            var cat = self.currentCatObject();
+            self.elemInputName.value = cat.name;
+            self.elemInputImage.value = cat.image;
+            self.elemInputCount.value = cat.clickCount;
+        }
+    });
 
-        getSelectedCat: function () {
-            return data.cats[data.selectedCat];
-        },
+    this.catImage = ko.computed(function () {
+        return this.currentCatObject().image;
+    }, this);
 
-        getCurrentIndex: function () {
-            return data.selectedCat;
-        },
+    this.clickCount = ko.computed(function () {
+        return this.currentCatObject().clickCount;
+    }, this);
 
-        addClickCount: function () {
-            var currentCat = data.cats[data.selectedCat];
-            currentCat.clickCount++;
-            viewCatDetail.updateClickCount();
+    this.catName = ko.computed(function () {
+        return this.currentCatObject().name;
+    }, this);
 
-            if (this.isShowAdmin() === true) {
-                viewAdmin.renderCount(currentCat.clickCount);
-            }
-        },
-
-        isShowAdmin: function () {
-            return data.bShowAdmin;
-        },
-
-        showAdmin: function (bShow) {
-            if (data.bShowAdmin === bShow) {
-                return;
-            }
-
-            data.bShowAdmin = bShow;
-            viewAdmin.render();
-        },
-
-        setInfo: function (name, image, count) {
-            if (name && image && count) {
-                var currentCat = this.getSelectedCat();
-                currentCat.name = name;
-                currentCat.image = image;
-                currentCat.clickCount = count;
-
-                data.bShowAdmin = false;
-                viewCatDetail.render();
-                viewAdmin.render();
-                viewCatList.render();
-            }
+    this.adminClick = function () {
+        if (this.showAdmin() === false) {
+            this.showAdmin(true);
         }
     };
 
-    var viewCatList = {
-        init: function () {
-            this.listTemplate = $('script[data-template="cat-list"').html();
+    this.saveClick = function () {
+        var name = this.elemInputName.value;
+        var image = this.elemInputImage.value;
+        var count = parseInt(this.elemInputCount.value);
 
-            $('#cat-list').on('change', 'input[type="radio"]', function (event) {
-                var index = this.getAttribute('value');
-                octopus.chooseCat(index);
-            });
-
-            this.render();
-        },
-
-        render: function () {
-            var list = [];
-            var that = this;
-
-            octopus.getAllCats().forEach(function (cat, index) {
-                var cat = that.listTemplate.replace(/{{name}}/g, cat.name)
-                    .replace(/{{value}}/g, index);
-                list.push(cat);
-            });
-
-            $('#cat-list').html(list.join(''));
-
-            $('input:radio[name=cat]').filter('[value=' + octopus.getCurrentIndex() + ']').prop('checked', true);
+        if (name && image && count) {
+            var cat = this.currentCatObject();
+            cat.name = name;
+            cat.image = image;
+            cat.clickCount = count;
+            this.catList()[this.currentCat()].valueHasMutated();
+            this.showAdmin(false);
         }
     };
 
-    var viewCatDetail = {
-        init: function () {
-            this.clickCount = document.getElementById('click-count');
-            this.image = document.getElementById('cat-image');
-            this.catName = document.getElementById('cat-name');
-
-            document.getElementById('cat-clicker').addEventListener('click', function (event) {
-                octopus.addClickCount();
-            }, false);
-
-            this.render();
-        },
-
-        render: function () {
-            var cat = octopus.getSelectedCat();
-
-            this.catName.innerText = cat.name;
-            this.clickCount.innerText = cat.clickCount;
-            this.image.setAttribute('src', cat.image);
-        },
-
-        updateClickCount: function () {
-            var cat = octopus.getSelectedCat();
-            this.clickCount.innerText = cat.clickCount;
-        }
+    this.cancelClick = function () {
+        this.showAdmin(false);
     };
 
-    var viewAdmin = {
-        init: function () {
-            this.adminSettingBox = document.getElementById('admin-setting-box');
-            this.inputName = document.getElementById('set-name');
-            this.inputImage = document.getElementById('set-image');
-            this.inputCount = document.getElementById('set-count');
-
-            document.getElementById('btn-admin').addEventListener('click', function () {
-                if (octopus.isShowAdmin() === false) {
-                    octopus.showAdmin(true);
-                }
-            }, false);
-
-            document.getElementById('btn-save').addEventListener('click', function () {
-                var name = viewAdmin.inputName.value;
-                var image = viewAdmin.inputImage.value;
-                var count = viewAdmin.inputCount.value;
-
-                if (name && image && count) {
-                    octopus.setInfo(name, image, count);
-                }
-            }, false);
-
-            document.getElementById('btn-cancel').addEventListener('click', function () {
-                octopus.showAdmin(false);
-            }, false);
-
-            this.render();
-        },
-
-        render: function () {
-            if (octopus.isShowAdmin() === true) {
-                this.adminSettingBox.removeAttribute('hidden');
-
-                var currentCat = octopus.getSelectedCat();
-                this.inputName.value = currentCat.name;
-                this.inputImage.value = currentCat.image;
-                this.inputCount.value = currentCat.clickCount;
-            } else {
-                this.adminSettingBox.setAttribute('hidden', '');
-            }
-
-        },
-
-        renderCount: function (count) {
-            this.inputCount.value = count;
-        }
+    this.addClickCount = function () {
+        var cat = this.currentCatObject();
+        cat.clickCount++;
+        this.catList()[this.currentCat()].valueHasMutated();
     };
+};
 
-    octopus.init();
-})();
+ko.applyBindings(new appViewModel());

00b41aca14d25a8be23d0e3fa2768c1b11a120e5
organism <neuqzhj@live.com>
2017-07-10 13:49:08 +0800

    refactor: Use model to manage data

--- js/script.js
+++ js/script.js
@@ -1,79 +1,69 @@
-(function () {
-    var CatsModel = function () {
+$(function () {
+    var initialCats = [{
+            firstName: 'Black And White',
+            lastName: 'Kitten On Brown Textile',
+            nicknames: ['Tabtab', 'T-Bone', 'Mr. T', 'Tabitha Tab Tabby Catty Cat'],
+            imageSrc: 'images/black-and-white-kitten-on-brown-textile.jpg',
+            imageAttribution: 'Pexels',
+            clickCount: 0
+        },
+        {
+            firstName: 'Eyes On You',
+            lastName: 'Cat',
+            nicknames: ['Tigger'],
+            imageSrc: 'images/eyes-on-you-cat.jpg',
+            imageAttribution: 'Pexels',
+            clickCount: 0
+        },
+        {
+            firstName: 'Grey And White',
+            lastName: 'Short Fur Cat',
+            nicknames: ['Casper'],
+            imageSrc: 'images/grey-and-white-short-fur-cat.jpg',
+            imageAttribution: 'Pexels',
+            clickCount: 0
+        },
+        {
+            firstName: 'Kittens',
+            lastName: 'Rush',
+            nicknames: ['Shooby'],
+            imageSrc: 'images/kittens-cat-cat-puppy-rush.jpg',
+            imageAttribution: 'Pexels',
+            clickCount: 0
+        },
+        {
+            firstName: 'Silver',
+            lastName: 'Tabby Cat',
+            nicknames: ['Zzzzz'],
+            imageSrc: 'images/silver-tabby-cat.jpg',
+            imageAttribution: 'Pexels',
+            clickCount: 0
+        },
+        {
+            firstName: 'Two',
+            lastName: 'Yellow Cat',
+            nicknames: [],
+            imageSrc: 'images/two-yellow-cat.jpg',
+            imageAttribution: 'Pexels',
+            clickCount: 0
+        }
+    ];
+
+    var Cat = function (catItem) {
         var self = this;
 
-        this.catList = ko.observableArray(
-            [
-                ko.observable({
-                    firstName: 'Black And White',
-                    lastName: 'Kitten On Brown Textile',
-                    name: function () {
-                        return this.firstName + ' ' + this.lastName;
-                    },
-                    image: 'images/black-and-white-kitten-on-brown-textile.jpg',
-                    clickCount: 0
-                }),
-                ko.observable({
-                    firstName: 'Eyes On You',
-                    lastName: 'Cat',
-                    name: function () {
-                        return this.firstName + ' ' + this.lastName;
-                    },
-                    image: 'images/eyes-on-you-cat.jpg',
-                    clickCount: 0
-                }),
-                ko.observable({
-                    firstName: 'Grey And White',
-                    lastName: 'Short Fur Cat',
-                    name: function () {
-                        return this.firstName + ' ' + this.lastName;
-                    },
-                    image: 'images/grey-and-white-short-fur-cat.jpg',
-                    clickCount: 0
-                }),
-                ko.observable({
-                    firstName: 'Kittens',
-                    lastName: 'Rush',
-                    name: function () {
-                        return this.firstName + ' ' + this.lastName;
-                    },
-                    image: 'images/kittens-cat-cat-puppy-rush.jpg',
-                    clickCount: 0
-                }),
-                ko.observable({
-                    firstName: 'Silver',
-                    lastName: 'Tabby Cat',
-                    name: function () {
-                        return this.firstName + ' ' + this.lastName;
-                    },
-                    image: 'images/silver-tabby-cat.jpg',
-                    clickCount: 0
-                }),
-                ko.observable({
-                    firstName: 'Two',
-                    lastName: 'Yellow Cat',
-                    name: function () {
-                        return this.firstName + ' ' + this.lastName;
-                    },
-                    image: 'images/two-yellow-cat.jpg',
-                    clickCount: 0
-                })
-            ]
-        );
+        self.firstName = ko.observable(catItem.firstName);
+        self.lastName = ko.observable(catItem.lastName);
+        self.nicknames = ko.observable(catItem.nicknames);
+        self.imageSrc = ko.observable(catItem.imageSrc);
+        self.clickCount = ko.observable(catItem.clickCount);
 
-        this.currentCat = ko.observable(0);
-        this.currentCat.subscribe(function () {
-            self.showAdmin(false);
+        self.fullName = ko.computed(function () {
+            return self.firstName() + ' ' + self.lastName();
         });
 
-        this.currentCatObject = ko.computed(function () {
-            return self.catList()[self.currentCat()]();
-        });
-
-        this.showAdmin = ko.observable(false);
-
-        this.level = ko.computed(function () {
-            var clickCount = self.currentCatObject().clickCount;
+        self.level = ko.computed(function () {
+            var clickCount = self.clickCount();
             var level = 'Newborn';
 
             if (3 <= clickCount && clickCount <= 5) {
@@ -86,73 +76,71 @@
 
             return level;
         });
-
-        this.catImage = ko.computed(function () {
-            return this.currentCatObject().image;
-        }, this);
-
-        this.clickCount = ko.computed(function () {
-            return this.currentCatObject().clickCount;
-        }, this);
-
-        this.catName = ko.computed(function () {
-            return this.currentCatObject().name();
-        }, this);
-
     };
-
 
     var AppViewModel = function () {
         var self = this;
 
-        this.elemInputFirstName = document.getElementById('set-first-name');
-        this.elemInputLastName = document.getElementById('set-last-name');
-        this.elemInputImage = document.getElementById('set-image');
-        this.elemInputCount = document.getElementById('set-count');
+        self.elemInputFirstName = document.getElementById('set-first-name');
+        self.elemInputLastName = document.getElementById('set-last-name');
+        self.elemInputImage = document.getElementById('set-image');
+        self.elemInputCount = document.getElementById('set-count');
 
-        this.cats = ko.observable(new CatsModel());
+        self.catList = ko.observableArray();
 
-        this.cats().showAdmin.subscribe(function (bShow) {
+        initialCats.forEach(function (catItem) {
+            self.catList.push(new Cat(catItem));
+        });
+
+        self.currentCat = ko.observable(self.catList()[0]);
+        self.currentCat.subscribe(function () {
+            self.showAdmin(false);
+        });
+
+        self.showAdmin = ko.observable(false);
+        self.showAdmin.subscribe(function (bShow) {
             if (bShow) {
-                var cat = self.cats().currentCatObject();
-                self.elemInputFirstName.value = cat.firstName;
-                self.elemInputLastName.value = cat.lastName;
-                self.elemInputImage.value = cat.image;
-                self.elemInputCount.value = cat.clickCount;
+                var cat = self.currentCat();
+                self.elemInputFirstName.value = cat.firstName();
+                self.elemInputLastName.value = cat.lastName();
+                self.elemInputImage.value = cat.imageSrc();
+                self.elemInputCount.value = cat.clickCount();
             }
         });
 
-        this.adminClick = function () {
-            if (this.showAdmin() === false) {
-                this.showAdmin(true);
+        self.trackClickCount = ko.computed(function () {
+            return self.currentCat().clickCount();
+        });
+
+        self.adminClick = function () {
+            if (self.showAdmin() === false) {
+                self.showAdmin(true);
             }
         };
 
-        this.saveClick = function () {
+        self.saveClick = function () {
             var firstName = self.elemInputFirstName.value;
             var lastName = self.elemInputLastName.value;
             var image = self.elemInputImage.value;
             var count = parseInt(self.elemInputCount.value);
 
             if (firstName && lastName && image && !isNaN(count)) {
-                var cat = this.currentCatObject();
-                cat.firstName = firstName;
-                cat.lastName = lastName;
-                cat.image = image;
-                cat.clickCount = count;
-                this.catList()[this.currentCat()].valueHasMutated();
-                this.showAdmin(false);
+                var cat = self.currentCat();
+                cat.firstName(firstName);
+                cat.lastName(lastName);
+                cat.imageSrc(image);
+                cat.clickCount(count);
+                self.showAdmin(false);
             }
         };
 
-        this.cancelClick = function () {
-            this.showAdmin(false);
+        self.cancelClick = function () {
+            self.showAdmin(false);
         };
 
-        this.addClickCount = function () {
-            var cat = this.currentCatObject();
-            cat.clickCount++;
-            this.catList()[this.currentCat()].valueHasMutated();
+        self.addClickCount = function () {
+            var cat = self.currentCat();
+            cat.clickCount(cat.clickCount() + 1);
         };
     };
 
